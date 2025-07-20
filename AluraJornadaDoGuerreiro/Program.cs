16ec87926ae9f402b01ad705b8ffeb8c62b1aaa6
Edilson Guimarães <eddy.g.jesus@gmail.com>
2025-07-19 22:17:28 -0300

    Adicionar arquivos de projeto.

---
+++ AluraJornadaDoGuerreiro/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program{
+
+    static void Main() {
+
+        int energia = 100;
+        string escolhas;
+        bool continuar = true;
+
+        do
+        {
+
+            Console.WriteLine("1 - Procura inimigos");
+            Console.WriteLine("2 - Ver energia");
+            Console.WriteLine("3 - Sair da floresta");
+            Console.Write("Escolha uma opção: ");
+
+            escolhas = Console.ReadLine()!;
+
+            switch (escolhas)
+            {
+                case "1":
+                    Console.WriteLine("Você encontrou um inimigo");
+                    energia -= 25;
+                    Console.WriteLine($"Energia atual:{energia}");
+                    break;
+                case "2":
+                    if (energia >= 50)
+                    {
+                        Console.WriteLine("Você ainda esta forte ");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Você esta cansado! Precisa descansar.");
+                    }
+                    break;
+
+                case "3":
+                    continuar = false;
+                    Console.WriteLine("Você saiu da floresta com segurança");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida, tente novamente.");
+                    break;
+
+            }
+
+        } while (continuar);
+    }
+
+ }
